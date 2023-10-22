5feaaef549b09470cbef1ada42545bd25c1c7830
Alessandro Scarozza <xan.scale@gmail.com>
2023-10-22 12:17:26 +0200

    fix MaskTransformation

--- transformations/src/main/java/com/commit451/coiltransformations/MaskTransformation.kt
+++ transformations/src/main/java/com/commit451/coiltransformations/MaskTransformation.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
 import android.graphics.drawable.Drawable
 import androidx.annotation.DrawableRes
 import androidx.core.content.res.ResourcesCompat
@@ -13,43 +14,41 @@ import androidx.core.graphics.createBitmap
 import coil.size.Size
 import coil.transform.Transformation
 import com.commit451.coiltransformations.Util.safeConfig
+import kotlin.math.max
 
 /**
  * Mask transformation using another drawable.
  * @param context context for loading the drawable
  * @param maskDrawableRes the drawable resource to use as the mask
  */
-class MaskTransformation(
-    private val context: Context,
-    @DrawableRes val maskDrawableRes: Int
-) : Transformation {
+class MaskTransformation(private val context: Context, @DrawableRes val maskDrawableRes: Int) : Transformation {
 
     companion object {
-        private val paint = Paint()
-            .apply {
-                xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-            }
+        private val paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG).apply {
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        }
     }
 
     override val cacheKey: String = "${MaskTransformation::class.java.name}-$maskDrawableRes"
 
-    override suspend fun transform(input: Bitmap, size: Size): Bitmap {
-
-        val output = createBitmap(input.width, input.height, input.safeConfig)
-        output.setHasAlpha(true)
-
-        val mask = getMaskDrawable(context.applicationContext, maskDrawableRes)
-
-        output.applyCanvas {
-            mask.setBounds(0, 0, width, height)
-            mask.draw(this)
-            drawBitmap(input, 0f, 0f, paint)
+    override suspend fun transform(input: Bitmap, size: Size) = getMaskDrawable(context, maskDrawableRes).let {
+        it.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+        createBitmap(it.intrinsicWidth, it.intrinsicHeight, input.safeConfig).applyCanvas {
+            it.draw(this)
+            val scale = max(it.intrinsicWidth.toFloat() / input.width, it.intrinsicHeight.toFloat() / input.height)
+            val scaledWidth = scale * input.width
+            val scaledHeight = scale * input.height
+            val left = (it.intrinsicWidth - scaledWidth) / 2
+            val top = (it.intrinsicHeight - scaledHeight) / 2
+            drawBitmap(input, null, RectF(left, top, left + scaledWidth, top + scaledHeight), paint)
         }
-        return output
     }
 
     private fun getMaskDrawable(context: Context, maskId: Int): Drawable {
         return ResourcesCompat.getDrawable(context.resources, maskId, null)
             ?: throw IllegalArgumentException("maskId is invalid")
     }
+
+    override fun equals(other: Any?) = (other as? MaskTransformation)?.maskDrawableRes == maskDrawableRes
+    override fun hashCode() = cacheKey.hashCode()
 }
