451fef81cdcdb2781bba85ec656c00b28a468217
John Carlson <jawnnypoo@gmail.com>
2024-11-08 01:49:09 -0600

    Back to false

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.maven.publish) apply false
 }
